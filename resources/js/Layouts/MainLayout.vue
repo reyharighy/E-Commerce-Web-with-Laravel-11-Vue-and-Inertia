9051650fb4cbb79399aa71335e705f991e35e5b1
reyharighy <arighymoch@gmail.com>
2024-12-02 21:05:03 +0700

    Create MainLayout
    this layout serves as a default apply to all pages

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,13 @@
+<script setup>
+import {ref} from 'vue'
+
+const timer = ref(0)
+setInterval(() => timer.value++, 1000)
+</script>
+
+<template>
+    <div>This is the layout serves as header to all pages.</div>
+    <div>The timer = {{ timer }}</div>
+
+    <slot>This is the slot</slot>
+</template>
