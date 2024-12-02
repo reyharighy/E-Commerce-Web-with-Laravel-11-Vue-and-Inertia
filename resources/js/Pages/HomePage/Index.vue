6c759efbe9654b27d932ca1f2428d64f7ab420d4
reyharighy <arighymoch@gmail.com>
2024-12-02 21:05:51 +0700

    Create a component for homepage component

---
+++ resources/js/Pages/HomePage/Index.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+    exampleProps: String
+})
+</script>
+
+<template>
+    <div>Home Page</div>
+    <div>
+        The message is "{{ exampleProps }}".
+    </div>
+</template>
