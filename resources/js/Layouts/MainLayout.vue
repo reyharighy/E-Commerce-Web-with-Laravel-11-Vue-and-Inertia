c965f80f652238b99334435f8c233d3025cc7ef8
reyharighy <arighymoch@gmail.com>
2024-12-03 20:50:47 +0700

    update the main layout and home page
    still nothing, need tailwind to proceed

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,13 +1,27 @@
 <script setup>
-import {ref} from 'vue'
-
-const timer = ref(0)
-setInterval(() => timer.value++, 1000)
+import {Link} from '@inertiajs/vue3'
 </script>
 
 <template>
-    <div>This is the layout serves as header to all pages.</div>
-    <div>The timer = {{ timer }}</div>
-
+    <nav>
+        <div>SHOP<span>.CO</span></div>
+        <ul>
+            <li><Link href="">Shop</Link></li>          <!--Undefined href-->
+            <li><Link href="">Top Sales</Link></li>     <!--Undefined href-->
+            <li><Link href="">Offers</Link></li>        <!--Undefined href-->
+            <li><Link href="">Seller</Link></li>>       <!--Undefined href-->
+        </ul>
+        <div>
+            <button>
+                <Link href=""><img src="" alt=""></Link>    <!--Undefined href and image-->
+            </button>
+            <input type="text" placeholder="Search for products...">
+        </div>
+        <div>
+            <Link><img src="" alt=""></Link>    <!--Undefined href and image-->
+            <Link><img src="" alt=""></Link>    <!--Undefined href and image-->
+            <label for="">The label</label>
+        </div>
+    </nav>
     <slot>This is the slot</slot>
 </template>
