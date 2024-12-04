3abff935396f6fc7e4fae34c342a7895b79d874c
reyharighy <arighymoch@gmail.com>
2024-12-05 05:12:43 +0700

    Established MainLayout
    simulate the vue components either for client-side and server-side. add the input for tailwind with app.css. define the homepage using HomeController with example props (change later)

--- resources/js/Pages/HomePage/Index.vue
+++ resources/js/Pages/HomePage/Index.vue
@@ -1,3 +1,12 @@
+<script setup>
+    defineProps({
+        message: String
+    })
+</script>
+
 <template>
-    <div>The content of the home page</div>
+    <div class="flex flex-col justify-center items-center">
+        <div>The content of the home page</div>
+        <div>This is the message -> {{ message }}</div>
+    </div>
 </template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -2,23 +2,23 @@ import '../css/app.css';
 import './bootstrap';
 
 import { createInertiaApp } from '@inertiajs/vue3';
-import { createApp, h } from 'vue';
+import { createSSRApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-import MainLayout from '@/Layouts/MainLayout.vue';
+import MainLayout from './Layouts/MainLayout.vue';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: () => `${appName}`,
     resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.vue', {eager : true})
+        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
         let page = pages[`./Pages/${name}.vue`]
         page.default.layout = page.default.layout || MainLayout
         
         return page
-    },
+      },
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+        return createSSRApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
             .mount(el);
